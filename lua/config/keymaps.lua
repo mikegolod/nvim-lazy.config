58ef3c09d45d44f78effa049a3e779fad6e2c9e8
Mikhail Golod <mikhail.golod@gmail.com>
2024-04-09 21:36:43 +0300

    add \de keymap to open DBUI

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local wk = require("which-key")
+
+-- DBUI
+-- vim.keymap.set("n", "<leader>8", "<cmd>DBUI<cr>", { desc = "Dadbod UI" })
+wk.register({
+  d = {
+    name = "Database",
+    e = { "<cmd>DBUI<cr>", "Explorer" },
+  },
+}, { prefix = "<leader>" })
